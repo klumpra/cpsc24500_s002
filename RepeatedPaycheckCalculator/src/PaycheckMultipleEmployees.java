2af7f6248426e4023dcd44322c44bec2b7b969d6
Ray Klump <klumpra@lewisu.edu>
2020-09-21 14:35:22 -0500

    Modified RepeatedPaycheckCalculator with do..while loop

--- RepeatedPaycheckCalculator/src/PaycheckMultipleEmployees.java
+++ RepeatedPaycheckCalculator/src/PaycheckMultipleEmployees.java
@@ -3,8 +3,8 @@ public class PaycheckMultipleEmployees {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double hours, rate, gross;
-		String doAgain = "y";
-		while (doAgain.equalsIgnoreCase("y")) {
+		String doAgain;
+		do {
 			System.out.print("Enter hours and rate: ");
 			hours = sc.nextDouble();
 			rate = sc.nextDouble();
@@ -12,7 +12,7 @@ public class PaycheckMultipleEmployees {
 			System.out.printf("Gross = $%.2f.\n", gross);
 			System.out.print("Again? ");
 			doAgain = sc.next(); 
-		}
+		} while (doAgain.equalsIgnoreCase("y"));
 		System.out.println("Thank you for using this program.");
 	}
 }
