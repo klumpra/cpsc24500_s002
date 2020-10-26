d1371effb8e0b83a908ea043360b2c86ca6a6306
Ray Klump <klumpra@lewisu.edu>
2020-10-26 14:51:54 -0500

    Finished StringPrinter

--- StringPrinterApp/src/StringPrinterApp.java
+++ StringPrinterApp/src/StringPrinterApp.java
@@ -1,34 +1,56 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 class TextPanel extends JPanel {
 	private String text;
-	
+//	private int fontSize;
+	private Font font;	
+
+	public Font getFont() {
+		return font;
+	}
+	public void setFont(Font font) {
+		this.font = font;
+	}
 	public String getText() {
 		return text;
 	}
 	public void setText(String t) {
 		text = t;
 	}
-	
+	public void setFont(int fs) {
+		if (fs < 6) {
+			font = new Font("Arial", Font.PLAIN, 12);
+		} else {
+			font = new Font("Arial", Font.PLAIN, fs);
+		}
+	}
 	public TextPanel() {
 		text = "Welcome!";
+		setFont(14);
 	}
 	
-	public TextPanel(String t) {
+	public TextPanel(String t, int fs) {
 		setText(t);
+		setFont(fs);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		g.setFont(font);
 		g.drawString(text, 20, 50);
 	}
 }
@@ -38,7 +60,7 @@ class StringPrinterFrame extends JFrame {
 		setTitle("String Printer App");
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
-		TextPanel panCenter = new TextPanel("Man I'm tired.");
+		TextPanel panCenter = new TextPanel("Man I'm tired.",14);
 		c.add(panCenter,BorderLayout.CENTER);
 		JPanel panSouth = new JPanel();
 		panSouth.setLayout(new FlowLayout());
@@ -50,6 +72,23 @@ class StringPrinterFrame extends JFrame {
 		panSouth.add(lblFontSize);
 		JTextField txtFontSize = new JTextField(2);
 		panSouth.add(txtFontSize);
+		JButton btnChange = new JButton("Change");
+		btnChange.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				panCenter.setText(txtText.getText());
+				try {
+					int fontSize = Integer.parseInt(txtFontSize.getText());
+					panCenter.setFont(fontSize);
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null,"The font size must be an integer.");
+				}
+				repaint(); // repaints the entire window's contents. For example, 
+						//panCenter will be repainted --> paintComponent for TextPanel
+						//will be automatically called.
+			}
+		}	
+		);
+		panSouth.add(btnChange);
 		c.add(panSouth,BorderLayout.SOUTH);
 	}
 	public StringPrinterFrame() {
