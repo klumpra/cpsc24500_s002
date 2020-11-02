20bb595c1707c186c865f3fdca5e33a97b91f59f
klumpra <klumpra@10.120.18.27>
2020-11-02 14:28:13 -0600

    Added ability to paint a dot where I click

--- MouseAndMenuApp/src/MouseAndMenuApp.java
+++ MouseAndMenuApp/src/MouseAndMenuApp.java
@@ -20,15 +20,22 @@ import javax.swing.JTextField;
 
 class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
 	private String mouseStatus;
+	private int x;   // x and y location of where I clicked
+	private int y;
 	public DrawingPanel() {
 		mouseStatus = "Welcome!";
 		addMouseListener(this);
 		addMouseMotionListener(this);
+		x = -1;   // anything negative will be treated as not
+		y = -1;   // something to be drawn
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawString(mouseStatus, 20, 20);
+		if (x >= 0 && y >= 0) {
+			g.fillOval(x-25,y-25,50,50);
+		}
 	}
 	public String getMouseStatus() {
 		return mouseStatus;
@@ -54,6 +61,8 @@ class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener 
 	}
 	public void mouseClicked(MouseEvent e) { 
 		mouseStatus = String.format("Mouse clicked at (%d, %d)", e.getX(), e.getY());
+		x = e.getX();
+		y = e.getY();
 		repaint();
 	}
 	public void mouseMoved(MouseEvent e) { 
