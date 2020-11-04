1a48b21a804d450cfb9bb26a5967dc891ec1943e
klumpra <klumpra@macbook-pro.lewisu.local>
2020-11-04 14:48:40 -0600

    Added drawing ability to MouseAndMenuApp.

--- MouseAndMenuApp/src/MouseAndMenuApp.java
+++ MouseAndMenuApp/src/MouseAndMenuApp.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,23 +19,93 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
-	private String mouseStatus;
-	private int x;   // x and y location of where I clicked
+class Dot {  // model class - just holds data
+	private int x;
 	private int y;
+	private int radius;
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getRadius() {
+		return radius;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public void setRadius(int rad) {
+		if (rad <= 0) {
+			radius = 1;  // a minimum radius of 3
+		} else {
+			radius = rad;
+		}
+	}
+	public Dot() {
+		x = 0;
+		y = 0;
+		radius = 1;
+	}
+	public Dot(int x, int y, int radius) {
+		setX(x);
+		setY(y);
+		setRadius(radius);
+	}
+	/* every class in Java belongs to the same family.
+	 * That family starts at java.lang.Object, the ulimate
+	 * ancestor of all. java.lang.Object introduced 11
+	 * functions that we inherit. One of those is
+	 * public String toString(). It returns a printable
+	 * String representation of the object that we can
+	 * customize. For example, we could System.out.println(a dot)
+	 * to see a text representation of it.
+	 */
+	@Override
+	public String toString() {
+		return String.format("%d %d %d", x, y, radius);
+	}
+}
+class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
+	// this is a View class
+	private String mouseStatus;
+	private ArrayList<Dot> dots;
+	private int dotSize;
+	public void clearDots() {
+		dots.clear();
+	}
+	public int getDotSize() {
+		return dotSize;
+	}
+	public void setDotSize(int ds) {
+		if (ds <= 0) {
+			dotSize = 1;
+		} else {
+			dotSize = ds;
+		}
+	}
+	public ArrayList<Dot> getDots() {
+		return dots;
+	}
+	public void setDots(ArrayList<Dot> listOfDots) {
+		dots = listOfDots;
+	}
 	public DrawingPanel() {
 		mouseStatus = "Welcome!";
 		addMouseListener(this);
 		addMouseMotionListener(this);
-		x = -1;   // anything negative will be treated as not
-		y = -1;   // something to be drawn
+		dots = new ArrayList<Dot>(); // avoid nullpointerexceptions
+		dotSize = 20;
 	}
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawString(mouseStatus, 20, 20);
-		if (x >= 0 && y >= 0) {
-			g.fillOval(x-25,y-25,50,50);
+		for (Dot dot : dots) {
+			g.fillOval(dot.getX(),dot.getY(),dot.getRadius(),dot.getRadius());
 		}
 	}
 	public String getMouseStatus() {
@@ -61,8 +132,6 @@ class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener 
 	}
 	public void mouseClicked(MouseEvent e) { 
 		mouseStatus = String.format("Mouse clicked at (%d, %d)", e.getX(), e.getY());
-		x = e.getX();
-		y = e.getY();
 		repaint();
 	}
 	public void mouseMoved(MouseEvent e) { 
@@ -72,6 +141,8 @@ class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener 
 	}
 	public void mouseDragged(MouseEvent e) { 
 		mouseStatus = String.format("Mouse dragged at (%d, %d)", e.getX(), e.getY());
+		Dot dotty = new Dot(e.getX(),e.getY(),dotSize);
+		dots.add(dotty);
 		repaint();
 	}
 
@@ -86,6 +157,7 @@ class MenuAndMouseFrame extends JFrame {
 		miClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				pan.setMouseStatus("");
+				pan.clearDots();
 				repaint();
 			}
 		});
@@ -123,6 +195,12 @@ class MenuAndMouseFrame extends JFrame {
 		JButton btnChange = new JButton("Change");
 		btnChange.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					int ds = Integer.parseInt(txtSize.getText());
+					pan.setDotSize(ds);
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null,"The size must be an int.");
+				}
 			}
 		});
 		panSouth.add(btnChange);
@@ -138,5 +216,9 @@ public class MouseAndMenuApp {
 	public static void main(String[] args) {
 		MenuAndMouseFrame frm = new MenuAndMouseFrame();
 		frm.setVisible(true);
+		Dot defDot = new Dot();  // default constructor call
+		Dot custDot = new Dot(42,13,24);
+		System.out.println(defDot);
+		System.out.println(custDot);
 	}
 }
