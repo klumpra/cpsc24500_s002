ab232db4b03e246a64e9b9fdf960bc9e116cc26d
klumpra <klumpra@localhost>
2020-11-06 14:49:24 -0600

    Forgot to post DotReader in that last push.

--- MouseAndMenuApp/src/DotReader.java
+++ MouseAndMenuApp/src/DotReader.java
@@ -1,4 +1,11 @@
+import java.io.File;
 
 public class DotReader {
-
+	public ArrayList<Dot> readFromText(String fname) {
+		File f = new File(fname);
+		return readFromText(f);
+	}
+	public ArrayList<Dot> readFromText(File f) {
+		// where the work will happen
+	}
 }
